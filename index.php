6c6dadeda7dacbb0e32c1e763be6e0b4e2d15b06
JBxss <juanseb100@gmail.com>
2023-09-08 00:10:15 -0500

    [ADD] metodo POST

--- index.php
+++ index.php
@@ -31,12 +31,40 @@ Flight::route('GET /clientes/@id', function ($id) {
     $query->execute([":id" => $id]);
     $data = $query->fetch();
 
-    $array[] = [
+    $array = [
         "Cedula" => $data['cedula_cliente'],
         "Nombre" => $data['nombre_cliente'],
         "Celular" => $data['celular_cliente'],
         "Correo" => $data['correo_cliente']
     ];
+
+    Flight::json($array);
+});
+
+Flight::route('POST /clientes/', function () {
+    $db = Flight::db();
+    $cedula = Flight::request()->data->cedula_cliente;
+    $nombre = Flight::request()->data->nombre_cliente;
+    $celular = Flight::request()->data->celular_cliente;
+    $correo = Flight::request()->data->correo_cliente;
+    $query = $db->prepare("INSERT INTO tbl_clientes (cedula_cliente, nombre_cliente, celular_cliente, correo_cliente) VALUES (:cedula, :nombre, :celular, :correo)");
+
+    $array = [
+        "error" => "Hubo un error al agregar los registros",
+        "status" => "Error"
+    ];
+
+    if ($query->execute([":cedula" => $cedula, ":nombre" => $nombre, ":celular" => $celular, ":correo" => $correo])) {
+        $array = [
+            "data" => [
+                "Cedula" => $cedula,
+                "Nombre" => $nombre,
+                "Celular" => $celular,
+                "Correo" => $correo
+            ],
+            "status" => "success"
+        ];
+    };
 
     Flight::json($array);
 });
