53898f687ff93e2b9ae34c092188b91bcf02b1b9
JBxss <juanseb100@gmail.com>
2023-09-09 00:16:02 -0500

    [ADD] Metodos compras

---
+++ classes/Compras.php
@@ -0,0 +1,44 @@
+<?php
+
+require 'vendor/autoload.php';
+
+class Compras
+{
+
+    function nuevaCompra()
+    {
+
+        $db = Flight::db();
+        $codigo = Flight::request()->data->codigo_producto;
+        $cedula = Flight::request()->data->cedula_cliente;
+        $fecha = Flight::request()->data->fecha_compra;
+        $query = $db->prepare("INSERT INTO tbl_compra (codigo_producto, cedula_cliente, fecha_compra) VALUES (:codigo, :cedula, :fecha)");
+
+        $array = [
+            "error" => "Hubo un error al agregar los registros",
+            "status" => "Error"
+        ];
+
+        if ($query->execute([":codigo" => $codigo, ":cedula" => $cedula, ":fecha" => $fecha])) {
+            $array = [
+                "Nuevo_Compra" => [
+                    "Codigo" => $codigo,
+                    "Cedula" => $cedula,
+                    "Fecha" => $fecha
+                ],
+                "status" => "success"
+            ];
+        };
+
+        Flight::json($array);
+    }
+
+    function calcularCompra()
+    {
+
+        $db = Flight::db();
+        $cedula = Flight::request()->data->cedula_cliente;
+        $fecha = Flight::request()->data->fecha_compra;
+
+    }
+}
--- index.php
+++ index.php
@@ -3,13 +3,17 @@
 require 'vendor/autoload.php';
 require 'classes/Clients.php';
 require 'classes/Products.php';
+require 'classes/Compras.php';
 
 Flight::register('db', 'PDO', array('mysql:host=localhost;dbname=APIMarket', 'root', ''));
 
 $clients = new Clients();
 $products = new Products();
+$compras = new Compras();
 
 Flight::route('POST /clientes', [$clients, 'resgistrarCliente']);
 Flight::route('POST /productos', [$products, 'crearProducto']);
+Flight::route('POST /compras', [$compras, 'nuevaCompra']);
+Flight::route('POST /calcular', [$compras, 'calcularCompra']);
 
 Flight::start();
